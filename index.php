acbc941b3fde81019081e423363b8c4f81b73192
strivegfx <strivegfx@gmail.com>
2013-07-02 21:42:49 +1200

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE>
+
+<html>
+
+	<head>
+
+		<title>Instagram Button</title>
+
+		<link type="text/javascript" rel="stylesheet" href="css/main.css">
+
+	</head>
+
+	<body>
+
+		<div class="wrapper">
+
+			<a class="instagram-button"></a>
+
+		</div>
+
+	</body>
+
+</html>
