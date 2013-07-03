86b10a093043546c17640af3986fdc8f497dc601
strivegfx <strivegfx@gmail.com>
2013-07-03 20:48:08 +1200

    Added in pseudo elements
    If I had time I would concat some icon fonts onto the main typeface and
    add them into the pseudo
    element content

--- index.php
+++ index.php
@@ -6,17 +6,40 @@
 
 		<title>Instagram Button</title>
 
-		<link type="text/javascript" rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/main.css">
 
 	</head>
 
 	<body>
 
-		<div class="wrapper">
+		<div class="wrapper clear-fix">
 
-			<a class="instagram-button"></a>
+			<!--<a class="instagram-button"></a>-->
 
 		</div>
+
+		<script src="js/jquery-1.8.3.min.js"></script>
+
+		<script>
+
+			// create a stack of buttons
+
+			(function(){
+
+				var $html = '',
+					$i = null;
+
+				for($i = 0; $i < 10; $i++){
+
+					$html += '<a class="instagram-button" style="background-image: url(http://placekitten.com/200/200)"></a>';
+
+				}
+
+				$('.wrapper').html($html);
+
+			})();
+
+		</script>
 
 	</body>
 
